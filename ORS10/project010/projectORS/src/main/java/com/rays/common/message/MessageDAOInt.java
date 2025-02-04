61246674fbd9caf6983a04e73fa9dc6bf650b41b
sk8704318 <sk8704318@gmail.com>
2025-02-04 19:06:12 +0530

    pass

---
+++ ORS10/project010/projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * Sanjay Kumar 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
